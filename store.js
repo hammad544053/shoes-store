f96bcc366cddf5af963c2a1487278eb2c3431c7c
Hammad Ullah <hk0448455@gmail.com>
2024-03-17 06:24:56 +0500

    commit

---
+++ store.js
@@ -0,0 +1,11 @@
+// Get the navbar
+var navbar = document.querySelector(".navbar");
+
+// Get the hamburger icon
+var hamburger = document.querySelector(".hamburger");
+
+// Add a click event listener to the hamburger icon
+hamburger.addEventListener("click", function() {
+  // Toggle the "responsive" class on the navbar
+  navbar.classList.toggle("responsive");
+});
